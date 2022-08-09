e1fe6fa1d495f5f254b54bd568117a3dc590c03c
rrrcoba <roger-decroly@hotmail.com>
2022-08-09 13:42:29 +0200

    Task 23 part 3 (basic STS4 grandle project)

---
+++ TA23_gradle/src/main/java/com/example/demo/Ta23GradleApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ta23GradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ta23GradleApplication.class, args);
+	}
+
+}
